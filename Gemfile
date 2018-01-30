4e83fe3fb793df9cc72f867e1dd6faa9fa919908
Nikolaos <lunaticnick@hotmail.com>
2018-01-30 12:47:33 +0000

    Create Gemfile and use simplecov -- fix existing test

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+ruby '2.4.2'
+
+gem 'capybara'
+gem 'rake'
+gem 'rspec'
+gem 'rubocop'
+gem 'simplecov', require: false, group: :test
+gem 'simplecov-console', require: false, group: :test
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,7 +6,7 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1

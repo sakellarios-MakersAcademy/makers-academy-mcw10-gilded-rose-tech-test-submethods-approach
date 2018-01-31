00ec8bd28decb9ae6064766312ce3b3da13d969a
Nikolaos <lunaticnick@hotmail.com>
2018-01-31 13:51:45 +0000

    Added #aged_brie & #backstage_pass

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,8 +18,42 @@ class GildedRose
     end
   end
 
+  def aged_brie()
+    @items.each do |item|
+        if item.name == "Aged Brie"
+          if item.quality <50
+            item.quality += 1
+            item.sell_in -= 1
+          else
+            item.quality = 50
+            item.sell_in -= 1
+        end
+
+    end
+    end
+  end
+
+  def backstage_pass
+    @items.each do |item|
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.sell_in.between?(6,10)
+          item.quality += 2
+        end
+        if item.sell_in.between?(1,5)
+          item.quality += 3
+        end
+        if item.sell_in <= 0
+          item.quality = 0
+        end
+      end
+
+    end
+  end
 
 
+
+
+# ======= OLD CODE ======
 
   def update_quality()
     @items.each do |item|
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -13,24 +13,28 @@ describe GildedRose do
       #   expect(items[0].name).to eq('foo')
       # end
 
+# DONE
       it 'decreases quality of normal items by 1 each day' do
         items = [Item.new('foo', 1, 1)]
         GildedRose.new(items).normal_item()
         expect(items[0].quality).to eq(0)
       end
 
+# NOT DONE
       it "does not decrease quality of normal items past 0" do
         items = [Item.new("foo", 1, 0)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq(0)
       end
 
+# DONE
       it "decreases quality of normal twice as fast once sell by date has passed" do
         items = [Item.new("foo", 0, 2)]
         GildedRose.new(items).normal_item()
         expect(items[0].quality).to eq(0)
       end
 
+# NOT DONE
       it "decreases the sell in date by 1 for normal items" do
         items = [Item.new("foo", 1, 1)]
         GildedRose.new(items).normal_item()
@@ -39,44 +43,52 @@ describe GildedRose do
     end
 
     context "Aged Brie" do
+# DONE
       it "increases quality of 'Aged Brie' by 1 each day" do
         items = [Item.new("Aged Brie", 1, 1)]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).aged_brie()
         expect(items[0].quality).to eq(2)
       end
 
+# DONE
       it "does not increase quality of 'Aged Brie' past 50" do
         items = [Item.new("Aged Brie", 1, 50)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq(50)
       end
 
+# DONE
       it "decreases sell in date by 1 of 'Aged Brie'" do
         items = [Item.new("Aged Brie", 1, 50)]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).aged_brie()
         expect(items[0].sell_in).to eq(0)
       end
     end
 
     context "Backstage Pass" do
+
+# DONE
       it "increases quality of backstage pass by 2 once sell by date within 10 days" do
         items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).backstage_pass()
         expect(items[0].quality).to eq(12)
       end
 
+# DONE
       it "increases qualilty of backstage pass by 3 once sell by date within 5 days" do
         items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 5)]
-        GildedRose.new(items).update_quality()
+        GildedRose.new(items).backstage_pass()
         expect(items[0].quality).to eq(8)
       end
 
+# NOT DONE
       it "changes quality of backstage pass to 0 once sell by date is 0" do
         items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 5)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq(0)
       end
 
+# NOT DONE
       it "decreases sell in date by 1 of backstage pass" do
         items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 5)]
         GildedRose.new(items).update_quality()
@@ -85,12 +97,15 @@ describe GildedRose do
     end
 
     context "Sulfuras" do
+
+# NOT DONE
       it "does not change the quality of 'Sulfuras'" do
         items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 5)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq(5)
       end
 
+# NOT DONE
       it "does not change the sell in date of 'Sulfuras'" do
         items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 5)]
         GildedRose.new(items).update_quality()

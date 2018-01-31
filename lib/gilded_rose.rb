ba8e0ef84f35fdc48c0b54b3a1580ad868b1719e
Nikolaos <lunaticnick@hotmail.com>
2018-01-31 11:52:43 +0000

    Remove magic numbers

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,28 +1,33 @@
 class GildedRose
 
+  MAXIMUM_QUALITY = 50
+  MINIMUM_QUALITY = 0
+
   def initialize(items)
     @items = items
   end
 
+
+
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
+        if item.quality > MINIMUM_QUALITY
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
       else
-        if item.quality < 50
+        if item.quality < MAXIMUM_QUALITY
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
+              if item.quality < MAXIMUM_QUALITY
                 item.quality = item.quality + 1
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
+              if item.quality < MAXIMUM_QUALITY
                 item.quality = item.quality + 1
               end
             end
@@ -32,10 +37,10 @@ class GildedRose
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
-      if item.sell_in < 0
+      if item.sell_in < MINIMUM_QUALITY
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
+            if item.quality > MINIMUM_QUALITY
               if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
               end
@@ -44,7 +49,7 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
+          if item.quality < MAXIMUM_QUALITY
             item.quality = item.quality + 1
           end
         end
